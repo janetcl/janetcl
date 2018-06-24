02ec87ab6ffe3c6055b27f375143331b2b671ec8
Janet Lee <janetlee@Janets-MacBook-Pro.local>
2018-06-24 14:18:51 -0300

    clear -m
    done

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ var morgan = require('morgan')
 var path = require('path')
 var app = express()
 
+// Sends static files from the public path directory
+app.use(express.static(path.join(__dirname, '/public')))
+// Use morgan to log request in dev mode
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+var port = config.APP_PORT || 4000
+app.listen(port) // Listen on port defined in config file
+
 
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  DB: 'mongodb://localhost:27017/janetcl.github.io',
+  APP_PORT: 4000
+}
